c66f903350570ab2ff29f84606d9c8840bb39af6
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-21 22:06:52 -0600

    Gabriel Ernesto Obando García - Ejercicio 6 fínalizado

--- src/Ejercicio6TranspuestaNumeros.java
+++ src/Ejercicio6TranspuestaNumeros.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Ejercicio6TranspuestaNumeros {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double[][] numeros = new double[3][3];
+
+        for (int fila=0; fila < numeros.length; fila++) {
+            for (int columna = 0; columna < numeros[fila].length; columna++) {
+                System.out.println("Ingrese un numero a registrar: ");
+                numeros[fila][columna] = teclado.nextDouble();
+            }
+        }
+
+        //invertir for o println
+        for (int fila = 0; fila < numeros.length; fila++) {
+            for (int columna = 0; columna < numeros[fila].length; columna++) {
+                System.out.println(numeros[columna][fila]);
+            }
+        }
+
+        teclado.close();
+    }
 }
