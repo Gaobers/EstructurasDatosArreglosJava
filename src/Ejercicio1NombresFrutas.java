f460e80744930f42754220df09d5b62da13a2be1
Rosaura <karlarosauras@gmail.com>
2026-05-21 19:53:36 -0600

    Karla Rosaura Sánchez Hernández Se desarrollo el ejercicio1 que permita almacenar el nombre de 10 frutas, en un vctor llamado "frutas" y que su contenido fuera sea de manera inversa.

--- src/Ejercicio1NombresFrutas.java
+++ src/Ejercicio1NombresFrutas.java
@@ -1,5 +1,24 @@
-public class Ejercicio1NombresFrutas {
-    public static void main(String[] args) {
+   import java.util.Scanner;
 
+    public class Ejercicio1NombresFrutas {
+        public static void main(String[] args) {
+
+            String[] frutas = new String[10];
+            Scanner teclado = new Scanner(System.in);
+
+            System.out.println("--- Registro de Frutas ---");
+
+            for (int i = 0; i < frutas.length; i++) {
+                System.out.print("Ingresa la fruta número " + (i + 1) + ": ");
+                frutas[i] = teclado.nextLine();
+            }
+
+            System.out.println("\n--- Mostrando las frutas en orden inverso ---");
+
+            for (int i = frutas.length - 1; i >= 0; i--) {
+                System.out.println("Posición [" + i + "]: " + frutas[i]);
+            }
+
+            teclado.close();
+        }
     }
-}
