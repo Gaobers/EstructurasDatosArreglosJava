342381bd8acd34fa58159d83703a8346929b99ae
Rosaura <karlarosauras@gmail.com>
2026-05-21 21:00:21 -0600

    Karla Rosaura Sánchez Hernández Se desarrollo el ejercicio3 que permite almacenar 12 números enteros en un vector llamado “números” y que posteriormente muestre la cantidad de números pares e impares obtenidos

--- src/Ejercicio3NumerosParesEImpares.java
+++ src/Ejercicio3NumerosParesEImpares.java
@@ -1,2 +1,34 @@
-public class Ejercicio3NumerosParesEImpares {
+import java.util.Scanner;
+
+public class Ejercicio3NumerosParesEImpares  {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[12];
+        Scanner teclado = new Scanner(System.in);
+
+        int pares = 0;
+        int impares = 0;
+
+        System.out.println("--- Registro de 12 Números ---");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            numeros[i] = teclado.nextInt();
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            if (numeros[i] % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+
+        System.out.println("\n--- Conteo Final ---");
+        System.out.println("Cantidad de números PARES: " + pares);
+        System.out.println("Cantidad de números IMPARES: " + impares);
+
+        teclado.close();
+    }
 }
